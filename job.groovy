7c3668e5b2f640b2313d50bcf277790edc5ad705
Pema Geyleg <pema.geyleg@gmail.com>
2017-05-26 10:32:55 -0400

    Adds Jobs.groovy

---
+++ job.groovy
@@ -0,0 +1,18 @@
+pipelineJob('Intake') {
+  triggers {
+  scm 'H/5 * * * *'
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url("${GIT_URL}")
+          }
+          branch('master')
+        }
+      }
+    scriptPath('Jenkinsfile')
+    }
+  }
+}
